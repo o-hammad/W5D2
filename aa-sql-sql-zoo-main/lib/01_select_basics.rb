5d0aefa069ea0a1d2d6d5e0de0af46c6e80c1f07
“Eltion <ebehrami01@gmail.com>
2023-09-19 15:09:31 -0400

    select nobel almost complete

--- aa-sql-sql-zoo-main/lib/01_select_basics.rb
+++ aa-sql-sql-zoo-main/lib/01_select_basics.rb
@@ -77,5 +77,11 @@ def just_the_right_size
   # with an area between 200,000 and 250,000 square kilometers.
   # BETWEEN allows range checking - note that it is inclusive.
   execute(<<-SQL)
+  SELECT 
+    name, (area/1000) AS area_in_1000s
+  FROM 
+    countries 
+  WHERE 
+    area BETWEEN 200000 AND 250000;
   SQL
 end
